15966588c6b4f8057176319429d9df70e0b908b4
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-08-03 03:59:59 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/gainvault/', // 👈 Required for GitHub Pages
   server: {
     proxy: {
       '/api': {
