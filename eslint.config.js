cb2a876382767a82ccb4429224582ba1d665683f
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-08-10 20:45:36 +0530

    .

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default [
   [{server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Change to your actual backend server
+        target: 'https://gainvault-capital.onrender.com', // Change to your actual backend server
         changeOrigin: true,
         secure: false,
       },
