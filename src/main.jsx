302d060a25b7e7ed68517d4e0bb0e7b11da688f1
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-08-10 20:06:25 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/gainvault">
+    <BrowserRouter basename="/">
       <AuthProvider>
         <App />
       </AuthProvider>
