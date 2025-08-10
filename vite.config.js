f954deb90133cc3f91b2b4b0ba0f011b58d16e86
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-08-10 20:02:13 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.RENDER ? '/' : '/gainvault/', // 👈 Required for GitHub Pages
+  base: '/', // 👈 Required for GitHub Pages
   server: {
     proxy: {
       '/api': {
