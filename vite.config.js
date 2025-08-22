ecd6ecd7cbad9d3d8af73516772bff8dce165cb8
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-08-23 03:54:08 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -17,6 +18,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       buffer: 'buffer',
       process: 'process/browser',
     },
